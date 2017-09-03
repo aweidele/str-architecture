8cf722440aafbf889deb805330c42b320c5be0c9
Aaron Weidele <aweidele@gmail.com>
2017-09-03 12:10:08 -0400

    the custom field

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 get_header();
+if(have_posts()) : while(have_posts()) : the_post();
+$content = get_field('modules');
 ?>
-
+<pre><?php print_r($content); ?></pre>
 <?php
+endwhile;
+endif;
 get_footer();
