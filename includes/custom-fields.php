7f369466a583cf3e39db46771aa30a0cacd7e6b1
Aaron Weidele <aweidele@gmail.com>
2017-09-14 19:29:05 -0400

    Add image orientation field

--- includes/custom-fields.php
+++ includes/custom-fields.php
@@ -477,6 +477,57 @@ acf_add_local_field_group(array (
 ));
 
 acf_add_local_field_group(array (
+	'key' => 'group_59bb0fdf70f44',
+	'title' => 'Images',
+	'fields' => array (
+		array (
+			'key' => 'field_59bb0fe49b371',
+			'label' => 'Orientation',
+			'name' => 'orientation',
+			'type' => 'select',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array (
+				'landscape' => 'Landscape',
+				'portrait' => 'Portrait',
+			),
+			'default_value' => array (
+				0 => 'landscape',
+			),
+			'allow_null' => 0,
+			'multiple' => 0,
+			'ui' => 0,
+			'ajax' => 0,
+			'return_format' => 'value',
+			'placeholder' => '',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'attachment',
+				'operator' => '==',
+				'value' => 'all',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+acf_add_local_field_group(array (
 	'key' => 'group_59987b535a254',
 	'title' => 'Project Sliders',
 	'fields' => array (
