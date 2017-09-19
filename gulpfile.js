f24a639013774164a58eec12f681738f0d2b5b7d
Aaron Weidele <aweidele@gmail.com>
2017-09-19 13:05:41 -0400

    Adding Gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: ['last 4 versions']
+};
+
+gulp.task('sass', function(){
+  return gulp
+    .src('src/sass/str.scss')
+    //.pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    //.pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('css/'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+});
