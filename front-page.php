afacbcd5a7a47db791f05f62d771e4d9f17f0406
Aaron Weidele <aweidele@gmail.com>
2017-10-01 19:11:33 -0400

    Making static grid

--- front-page.php
+++ front-page.php
@@ -1,14 +1,24 @@
 <?php
 get_header();
 if(have_posts()) : while(have_posts()) : the_post();
-$sketch = get_field('sketch');
+$sketch1 = get_field('sketch_tile_1');
+$sketch2 = get_field('sketch_tile_2');
 $mobile_sketch = get_field('mobile_sketch');
 $project_page = get_field('project_page');
+
+$sketch = [];
+for($i=1;$i<16;$i++) {
+  $field = 'sketch_tile_'.$i;
+  $sketch[] = get_field($field);
+}
 include('modules/str-homepage-sketch.php');
 ?>
 <div class="frontpage_mobile_sketch">
   <img src="<?php echo $mobile_sketch['url']; ?>">
 </div>
+<pre>
+<?php print_r($sketch); ?>
+</pre>
 <?php
 endwhile;
 endif;
