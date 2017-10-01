5e17d931fde21b5072b7d135be5a8dc3317fbdd8
Aaron Weidele <aweidele@gmail.com>
2017-10-01 15:14:01 -0400

    Add ACF for Sketch module

--- front-page.php
+++ front-page.php
@@ -1,9 +1,9 @@
 <?php
 get_header();
 if(have_posts()) : while(have_posts()) : the_post();
-$sketch = get_field('sketch');
+$sketch = get_field('mobile_sketch');
 ?>
-<div class="frontpage_sketch">
+<div class="frontpage_mobile_sketch">
   <img src="<?php echo $sketch['url']; ?>">
 </div>
 <?php
