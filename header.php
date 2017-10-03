52cda96c8dfbac718c4ac66d1c5568a0c17b527c
Aaron Weidele <aweidele@gmail.com>
2017-10-02 22:51:31 -0400

    Make the logo not 100% on frontpage load screen

--- header.php
+++ header.php
@@ -48,6 +48,7 @@ echo file_get_contents( get_stylesheet_directory_uri() . '/images/logo.svg' );
   if(is_front_page()) {
     $c = 'body_frontpage';
     $c .= $first_visit ? ' body_frontpage_load' : '';
+    //$c .= ' body_frontpage_load';
     echo ' class="'.$c.'"';
   }
 
