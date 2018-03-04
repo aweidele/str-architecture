e06eac4db5a99665538d57e628e42f0208d89e21
Aaron Weidele <aweidele@gmail.com>
2018-03-04 14:59:57 -0500

    news list

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 get_header();
-echo "HALLO!";
-if(have_posts()) : while(have_posts()) : the_post();
-$content = get_field('modules');
-foreach($content as $block) {
-  include('modules/str-'.$block['acf_fc_layout'].'.php');
-}
-endwhile;
-endif;
+?>
+<div class="row">
+  <div class="cell">
+<?php
+  if(have_posts()) : while(have_posts()) : the_post();
+?>
+  <article class="news_article">
+    <h1 class="news_title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <?php the_content(); ?>
+  </article>
+<?php
+  endwhile;
+  endif;
+?>
+  </div>
+</div>
+<?php
 get_footer();
