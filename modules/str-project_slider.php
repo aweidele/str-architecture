a710ece011e95fa7e6312796bb8dd3a5bbec1779
Aaron Weidele <>
2022-03-28 10:37:50 -0400

    something

--- modules/str-project_slider.php
+++ modules/str-project_slider.php
@@ -4,6 +4,7 @@ $block_slides = get_field('slides',$block['project']->ID);
 $block_text_slides = get_field('text_slides',$block['project']->ID);
 $block_description = get_field('description',$block['project']->ID);
 $block_name = $block['project']->post_name;
+$block_classlist = ['project-slider'];
 
 include('str_slides.php');
 $current_user = wp_get_current_user();
--- modules/str-slider.php
+++ modules/str-slider.php
@@ -2,5 +2,6 @@
 $block_slides = $block['slides'];
 $block_text_slides = $block['text_slides'];
 $block_description = $block['description'];
+$block_classlist = ['page-slider'];
 
 include('str_slides.php');
